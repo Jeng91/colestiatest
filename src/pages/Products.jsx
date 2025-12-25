66a48c418828a7c9a0e813f7ac5c8049a7d254b4
Jeng91 <your-email@example.com>
2025-12-26 02:14:27 +0700

    pro

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -118,7 +118,7 @@ const MovieCard = ({ movie }) => {
                         <span>เริ่ม: {formatDateThai(movie.startDate)} - สิ้นสุด: {formatDateThai(movie.endDate)}</span>
                     </div>
                     <div className="flex items-center gap-1">
-                        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" >
                             <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
                             <circle cx="9" cy="7" r="4"></circle>
                             <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
